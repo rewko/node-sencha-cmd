51186fd3c5085dc5d48dbbbb149cf2f62e98b44d
rewko <info@rewko.eu>
2015-02-10 14:07:06 +0100

    Upgraded to TypeScript 1.4 gulp-tsc replaced by gulp-typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,40 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    typescript = require('gulp-tsc');
+    ts = require('gulp-typescript');
 
-var isWatching = false,
-    production = true;
+var production = true;
 
 if (gutil.env.debug === true) {
     production = false;
 }
 
-gulp.task('default', ['test']);
+var tsProject = ts.createProject({
+    module: 'commonjs',
+    noImplicitAny: true,
+    removeComments: false
+});
+
+gulp.task('default', ['watch']);
 
 gulp.task('lib', function () {
     return gulp.src(['./lib/*.ts'])
-        .pipe(typescript({
+        .pipe(ts({
             module: 'commonjs',
-            sourcemap: !production,
-            removeComments: production,
-            emitError: !isWatching
+            noImplicitAny: true,
+            removeComments: false
         }))
         .pipe(gulp.dest('./lib'));
 });
 
 gulp.task('test', function () {
-    return gulp.src(['./test/*.ts'])
-        .pipe(typescript({
-            module: 'commonjs',
-            sourcemap: !production,
-            removeComments: production,
-            emitError: !isWatching
-        }))
+    return gulp.src([
+        './lib/*.ts',
+        './test/*.ts'
+    ], {base: './'})
+        .pipe(ts(tsProject))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', ['test'], function () {
-    isWatching = true;
-
     gulp.watch('./**/*.ts', ['test']);
 });
--- test/sanity.js
+++ test/sanity.js
@@ -1,17 +1,15 @@
+/// <reference path="../node.d.ts" />
 var sencha = require('../lib/runner');
-
 var cmd = 'd:/apps/Sencha/Cmd/5.0.0.160';
-
 function done() {
     console.log('done ...');
 }
-
+// which
 sencha.cmd(cmd, 'which').run().on('data', function (msg) {
     console.log(msg);
 }).on('end', done);
-
+// compile
 var compile = sencha.cmd(cmd, 'compile').option('-cl', '/src,../../../../Dashboard,../../../../Prototype').union('-r', '-c Dashboard.view.widget.popup.MessageDialogWindow').include('-r', '-cl Dashboard.view.widget.popup.DataDialogWindow').meta('requires.txt').option('--tpl', '{0}').option('--filenames').concatenate('single.ext.js');
-
 console.log(compile.toString());
-
+// app build
 console.log(sencha.cmd(cmd, 'app build').toString());
